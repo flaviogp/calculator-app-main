25e6298f75587f35d4a039e14e8daeb6b60dd1bb
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-01-25 11:02:15 -0300

    fix: fix keypad layout

--- src/components/Keypad/index.tsx
+++ src/components/Keypad/index.tsx
@@ -2,35 +2,33 @@
 const Keypad = () => {
   return (
     <div className="keypad">
-      <table>
-        <td>
-          <tr id="7">7</tr>
-          <tr id="4">4</tr>
-          <tr id="1">1</tr>
-          <tr id="dot">.</tr>
-        </td>
-        <td>
-          <tr id="8">8</tr>
-          <tr id="5">5</tr>
-          <tr id="2">2</tr>
-          <tr id="0">0</tr>
-        </td>
-        <td>
-          <tr id="9">9</tr>
-          <tr id="6"> 6</tr>
-          <tr id="3">3</tr>
-          <tr id="bar">/</tr>
-        </td>
-        <td>
-          <tr id="del">del</tr>
-          <tr id="plus">+</tr>
-          <tr id="minus">-</tr>
-          <tr id="multiplier">x</tr>
-        </td>
-      </table>
-      <div className="control-buttons">
-        <button id="reset">reset</button>
-        <button id="equal">=</button>
+      <div className="row">
+        <div id="7">7</div>
+        <div id="8">8</div>
+        <div id="9">9</div>
+        <div id="del">del</div>
+      </div>
+      <div className="row">
+        <div id="4">4</div>
+        <div id="5">5</div>
+        <div id="6">6</div>
+        <div id="plus">+</div>
+      </div>
+      <div className="row">
+        <div id="1">1</div>
+        <div id="2">2</div>
+        <div id="3">3</div>
+        <div id="minus">-</div>
+      </div>
+      <div className="row">
+        <div id="dot">.</div>
+        <div id="0">0</div>
+        <div id="bar">/</div>
+        <div id="multiplier">x</div>
+      </div>
+      <div className="row buttons">
+      <div id="reset">reset</div>
+      <div id="equal">=</div>
       </div>
     </div>
   )
